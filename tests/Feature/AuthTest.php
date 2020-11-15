9ee8aa7e9559d79be21e6f9a8194e63ad4443e75
armel <armelkepjio@yahoo.fr>
2020-11-15 23:33:35 +0000

    Create tests in the AuthTest.php file

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/authentification');
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Test Landing Page.
+     *
+     * @return void
+     */
+    public function testLandingPage()
+    {
+        $this->visit('/')
+             ->see('contactez nous')
+             ->see('index')
+             ->see('produits');
+    }
+
+    /**
+     * Test Login Page
+     *
+     * @return void
+     */
+    public function testLoginPage()
+    {
+        $this->visit('/authentification')
+            ->see('Page d\'authentification');
+    }
+}
