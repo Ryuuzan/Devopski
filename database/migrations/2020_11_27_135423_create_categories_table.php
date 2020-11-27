016b50323755840428c1e040cd45f7cd23b1bc6b
Ryuuzan <alexmilo96@wanadoo.com>
2020-11-27 15:20:32 +0100

    New database

--- database/migrations/2020_11_27_135423_create_categories_table.php
+++ database/migrations/2020_11_27_135423_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePasswordResetsTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email');
-            $table->string('token');
-            $table->timestamp('created_at');
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id_cat');
+            $table->text('cat_nom');
         });
     }
 
@@ -27,6 +26,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('categories');
     }
 }
