2135c8a11da107ed3d55277fb435a79e57b8551e
armel <armelkepjio@yahoo.fr>
2020-11-30 13:38:52 +0000

    Modify the unit test file 1

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,10 +3,17 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
+
+    use DatabaseMigrations;
+
     /**
      * A basic test example.
      *
@@ -18,4 +25,38 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    /**
+     * Test Login Page
+     *
+     * @return void
+     */
+    public function testLandingPageWithUserLogged()
+    {
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+            ->visit('/')
+            ->see('contactez nous')
+            ->see('produits')
+            ->see('index')
+            ->see($user->name);
+    }
+
+    /**
+     * Test Login
+     *
+     * @return void
+     */
+    public function testLogin()
+    {
+        $user = factory(App\User::class)->create(['password' => Hash::make('passw0RD')]);
+        
+        $this->visit('/authentification')
+            ->type($user->email, 'email')
+            ->type('passw0RD', 'password')
+            ->press('submit')
+            ->seePageIs('/Welcome')
+    }
+
 }
