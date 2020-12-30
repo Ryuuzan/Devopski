4d9e73b2e92238cbeaff095b5985f507b9092af5
armel <armelkepjio@yahoo.fr>
2020-12-30 19:59:10 +0000

    modify testfile

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -4,6 +4,11 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Support\Facades\Hash;
+use App\User;
+
 use Tests\TestCase;
 
 class AuthTest extends TestCase
@@ -31,4 +36,50 @@ class AuthTest extends TestCase
         $this->visit('/authentification')
             ->see('Page d\'authentification');
     }
+   
+
+use RefreshDatabase;                                
+use DatabaseMigrations;                             
+use WithoutMiddleware;     
+
+
+                        
+    /**                                                
+     * Test Login                                      
+     *                                                 
+     * @return void                                    
+     */
+    public function testLogin()
+    {
+
+        $user = \App\Models\User::factory(User::class)->create(['password' => Hash::make('passw0RD'),]);
+    
+        $this->visit('/authentification')
+             ->type($user->pseudo, 'pseudo')
+             ->type('passw0RD', 'mdp')
+             ->press('submit')
+             ->seePageIs('/authentification');
+    }
+
+    /**
+     * Test Login
+     *
+     * @return void
+     */
+    public function testLandingPageWithUserLogged()
+    {
+        $user = \App\Models\User::factory(User::class)->create();
+
+        $this->actingAs($user)
+            ->visit('/')
+            ->click('contactez nous')
+            ->seePageIs('/contact')
+            ->click('Produits')
+            ->seePageIs('/products')
+            ->click('Login')
+            ->seePageIs('/authentification');
+    }
+
+
+
 }
